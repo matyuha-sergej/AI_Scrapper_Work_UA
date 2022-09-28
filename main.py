03f720618f29ddc280571c91678e332902e6a91a
serhii <sergei.matuha@gmail.com>
2022-09-28 14:42:11 +0300

    generate url for search

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
+job_name ="Data"
+job_name2="Science"
+page_num = 1
 
+url = "https://www.work.ua/ru/jobs-{}+{}/?page={}" \
+    .format(job_name, job_name2, page_num)
 
 
 
 if __name__ == '__main__':
-    pass
+    print(url)
 
